deb3fa50c7d76824479f222f6234c88e69162174
madhav1602 <madhavaggarwal580@gmail.com>
2025-04-09 06:05:14 +0530

    [Madhav] Add: Finding rotation point

--- RotationPoint.java
+++ RotationPoint.java
@@ -2,9 +2,12 @@ import java.util.Scanner;
 
 public class RotationPoint {
     public static int rotatedIndex(int[] arr){
-        int left=0,right=arr.length-1;
+        int left=0;
+        int right=arr.length-1;
+
         while(left<right){
             int mid=(left+(right-left))/2;
+
             if(arr[mid]>arr[right]){
                 left=mid+1;
             }
